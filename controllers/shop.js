0aa720925fc175e61aef385c29073fc00c874585
lamees-mohamed <62202246+lamees-mohamed@users.noreply.github.com>
2020-04-04 18:43:04 +0200

    delete product from the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -69,3 +69,12 @@ exports.postCart = (req, res, next) => {
     });
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .removeFromCart(prodId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
--- routes/shop.js
+++ routes/shop.js
@@ -16,5 +16,7 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 
 module.exports = router;
